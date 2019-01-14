c2398505b6d2078293328b09a06fd87d1471d66d
LeGreed <axel.coenon@gmail.com>
2019-01-14 14:40:45 +0100

    view controller v2

---
+++ src/main/java/fr/formation/proxi4/Proxi4Constants.java
@@ -0,0 +1,7 @@
+package fr.formation.proxi4;
+
+public class Proxi4Constants {
+
+	public static final String REDIRECT_TO_INDEX = "redirect:/index.html";
+
+}
--- src/main/java/fr/formation/proxi4/presentation/ViewController.java
+++ src/main/java/fr/formation/proxi4/presentation/ViewController.java
@@ -1,6 +1,9 @@
 package fr.formation.proxi4.presentation;
 
+import java.time.LocalDate;
+
 import org.apache.log4j.Logger;
+import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import fr.formation.proxi4.Proxi4Constants;
 import fr.formation.proxi4.metier.entity.Survey;
 import fr.formation.proxi4.metier.service.AnswerService;
 import fr.formation.proxi4.metier.service.ClientService;
@@ -31,10 +35,12 @@ public class ViewController {
 	@Autowired
 	private AnswerService answerService;
 	
+	
 	@RequestMapping({ "", "index" })
 	public ModelAndView index(@RequestParam(required=false) String message) {
-		ModelAndView mav = new ModelAndView();
 		LOGGER.debug("Entrée sur la page index.");
+		LOGGER.debug("Message récupéré suite à un redirection : " + message);
+		ModelAndView mav = new ModelAndView();
 		mav.setViewName("index");
 		LOGGER.debug("Ajout du sondage actuel.");
 		mav.addObject("survey", this.surveyService.getCurrentSurvey());
@@ -44,22 +50,34 @@ public class ViewController {
 		
 	@RequestMapping("surveys")
 	public ModelAndView loadSurveys() {
-		ModelAndView mav = new ModelAndView();
 		LOGGER.debug("Entrée page des sondages.");
+		ModelAndView mav = new ModelAndView();
 		mav.setViewName("surveys");
 		LOGGER.debug("Chargement des sondages");
 		mav.addObject("surveys", this.surveyService.readAll());
 		return mav;
 	}
 	
-	public ModelAndView showSurvey() {
+	@RequestMapping("survey")
+	public ModelAndView showSurvey(@RequestParam Integer id) {
+		LOGGER.debug("Entrée sur la page Sandage");
 		ModelAndView mav = new ModelAndView();
+		mav.setViewName("survey");
+		LOGGER.debug("Lecture du sondage.");
+		Survey survey = this.surveyService.read(id);
+		LOGGER.debug("Hibernate.initialize.");
+		Hibernate.initialize(survey);
+		LOGGER.debug("Ajout du sondage au mav.");
+		mav.addObject("survey", survey);
 		return mav;
 	}
 	
-	
+	@RequestMapping("form")
 	public ModelAndView createSurvey() {
+		LOGGER.debug("Entrée sur le formulaire de création du Sondage");
 		ModelAndView mav = new ModelAndView();
+		mav.setViewName("form");
+		mav.addObject("survey", new Survey());
 		return mav;
 	}
 	
@@ -69,8 +87,24 @@ public class ViewController {
 		LOGGER.debug("Création du sondage en DB.");
 		this.surveyService.create(survey);
 		message = "Sondage ajouté";
-		LOGGER.debug("Renvoie du message");
-		return message;
-		
+		attributes.addFlashAttribute("message", message);
+		LOGGER.debug("Renvoi du message");
+		return Proxi4Constants.REDIRECT_TO_INDEX;
+	}
+	
+	@RequestMapping("close")
+	public String closeSurvey(Integer id, RedirectAttributes attributes) {
+		String message = null;
+		LOGGER.debug("Set du sondage pour lui ajouter une date de fin");
+		Survey survey = this.surveyService.read(id);
+		LOGGER.debug("Hibernate.initilaize.");
+		Hibernate.initialize(survey);
+		LOGGER.debug("Setting de la date.");
+		survey.setEndDate(LocalDate.now());
+		LOGGER.debug("Update du sondage.");
+		this.surveyService.update(survey);
+		message = "Sondgae terminé.";
+		attributes.addFlashAttribute("message", message);
+		return Proxi4Constants.REDIRECT_TO_INDEX;
 	}
 }
